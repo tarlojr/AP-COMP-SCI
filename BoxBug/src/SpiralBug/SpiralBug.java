e89f59b8da5214f7d287906311b185aac970e6c8
Jacob <tarlojr1@gmail.com>
2017-11-24 16:26:37 -0500

    Final

--- BoxBug/src/SpiralBug/SpiralBug.java
+++ BoxBug/src/SpiralBug/SpiralBug.java
@@ -1,25 +1,71 @@
 package SpiralBug;
+
+import info.gridworld.actor.Actor;
 import info.gridworld.actor.Bug;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
 
 public class SpiralBug extends Bug {
+
     private int steps;
     private int sideLength;
-    public void SpiralBug(int length){
+    private boolean x = false;
+    private int y = 0;
+
+    SpiralBug(int length) {
         steps = 0;
         sideLength = length;
     }
-    public void act(){
-        if(steps< sideLength && canMove()){
-            move();
-            turn();
-            turn();
-            steps++;
+
+    public void SpiralBug(int length) {
+        steps = 0;
+        sideLength = length;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public boolean canMove() {
+        Grid<Actor> gr = getGrid();
+        if (gr == null) {
+            return false;
         }
-        else{
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next)) {
+            return false;
+        }
+        Actor neighbor = gr.get(next);
+        return (neighbor == null);
+// ok to move into empty location or onto flower
+// not ok to move onto any other actor
+    }
+
+    @Override
+    public void act() {
+        if (steps < sideLength && canMove()) {
+            move();
+            steps++;
+        } 
+        else {
             turn();
             turn();
             steps = 0;
+            x = !x;
+            if(canMove() == false){
+            y++;
+        }
+            if(canMove() == true){
+            y = 0;
+        }
+            if(y>5){
+             removeSelfFromGrid();   
+            }
+            if (x == false) {
+                sideLength++;
+            }   
         }
     }
-    
 }
