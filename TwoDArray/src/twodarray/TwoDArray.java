eb838f1d838e0fe55e3d66fc7ad04f924fa20eb1
tarlojr <33183266+tarlojr@users.noreply.github.com>
2017-12-06 11:11:43 -0500

    Add files via upload

--- TwoDArray/src/twodarray/TwoDArray.java
+++ TwoDArray/src/twodarray/TwoDArray.java
@@ -1,27 +1,32 @@
 package twodarray;
 
 public class TwoDArray {
+
     int math;
     int[][] twoDArray;
-    public TwoDArray(int r, int c){
+    String toString = "";
+
+    public TwoDArray(int r, int c) {
         twoDArray = new int[r][c];
     }
-    public void display(){
-        if(twoDArray.length <= 10){
-                for(int x = 0; x < twoDArray.length; x++){
-                  if(x>0){
-                      System.out.print(", ");
-                  }
-                  System.out.print(twoDArray[x]);
-                }
-                System.out.println();
+
+    public void display() {
+        int y = 0;
+        for (int x = 0; x < twoDArray.length; x++) {
+            if (y < twoDArray[x].length) {
+                toString += twoDArray[x][y] + ", ";
+                y++;
             }
+        }
+        System.out.println(toString);
     }
-    public void randomize(){
-        for(int y = 0; y>twoDArray.length; y++){
-            int[] x = new int [1];
-            x[1] = x[(int)(Math.random()+1)*100];
-            twoDArray[y] = x;
+
+    public void randomize(int min, int max) {
+        for (int y = 0; y < twoDArray.length; y++) {
+
+            for (int u = 0; u < twoDArray[y].length; u++) {
+                twoDArray[y][u] = (int) (Math.random() * (max - min + 1)) + min;
+            }
         }
     }
 }
--- TwoDArray/src/twodarray/TwoDArrayMain.java
+++ TwoDArray/src/twodarray/TwoDArrayMain.java
@@ -2,10 +2,12 @@ package twodarray;
 
 public class TwoDArrayMain {
     public static void main(String[] args) {
-        int x = 2;
-        int y = 5;
+        int min = 1;
+        int max = 100;
+        int x = 7;
+        int y = 3;
         TwoDArray a = new TwoDArray(x, y);
-        a.randomize();
+        a.randomize(min, max);
         a.display();
     }
 }
