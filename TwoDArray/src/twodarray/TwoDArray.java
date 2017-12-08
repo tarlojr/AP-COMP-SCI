851584af37413a504211d5e50060b9908a456e8d
tarlojr <33183266+tarlojr@users.noreply.github.com>
2017-12-08 11:48:30 -0500

    Add files via upload

--- TwoDArray/src/twodarray/TwoDArray.java
+++ TwoDArray/src/twodarray/TwoDArray.java
@@ -3,30 +3,44 @@ package twodarray;
 public class TwoDArray {
 
     int math;
+    int sumRow;
     int[][] twoDArray;
     String toString = "";
+    String toStringRow = "";
 
     public TwoDArray(int r, int c) {
         twoDArray = new int[r][c];
     }
 
     public void display() {
-        int y = 0;
         for (int x = 0; x < twoDArray.length; x++) {
-            if (y < twoDArray[x].length) {
+            for(int y = 0; y < twoDArray[0].length; y++){
                 toString += twoDArray[x][y] + ", ";
-                y++;
             }
+            toString += "\n";
         }
         System.out.println(toString);
     }
 
     public void randomize(int min, int max) {
         for (int y = 0; y < twoDArray.length; y++) {
-
             for (int u = 0; u < twoDArray[y].length; u++) {
                 twoDArray[y][u] = (int) (Math.random() * (max - min + 1)) + min;
             }
         }
+    }
+    
+    private void sumEachRow(int x){
+            for(int y = 0; y < twoDArray[0].length; y++){
+                sumRow = sumRow + twoDArray[x][y];
+        }
+    }
+    public void sumRow(){
+        for(int x = 0; x < twoDArray.length; x++){
+            sumEachRow(x);
+        }
+    }
+    public void sumColum(){
+        
     }
 }
--- TwoDArray/src/twodarray/TwoDArrayMain.java
+++ TwoDArray/src/twodarray/TwoDArrayMain.java
@@ -4,10 +4,11 @@ public class TwoDArrayMain {
     public static void main(String[] args) {
         int min = 1;
         int max = 100;
-        int x = 7;
-        int y = 3;
+        int x = 2;
+        int y = 2;
         TwoDArray a = new TwoDArray(x, y);
         a.randomize(min, max);
         a.display();
+        System.out.println(a.sumRow);
     }
 }
